7f842459431db1485ab21d357f907debf939d2d0
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 02:43:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,8 @@
 # Write your code here.
-def line(katz_deli)
+def line(katz_deli, other_deli, another_deli)
   
   if katz_deli == []
     puts "The line is currently empty."
   end
 
-end
-
-def line(other_deli)
-  
-  if other_deli == 
-  end  
 end
