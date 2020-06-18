839f63352af91f9892137a6ebec19da5ca658dfb
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 03:04:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,20 @@
 def line(array)
   
   if array.length >= 1
+    
     new_array = []
+    
     counter = 1
+    
     array.each do | name |
       new_array.push("#{counter}. #{name}")
       counter += 1
     end
+    
     puts "The line is currently: #{new_array.join(" ")}"
   
   else
+    
     puts "The line is currently empty."
     
   end
