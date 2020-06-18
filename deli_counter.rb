cd7c961a08f611557bea5e5d6bd3e4ac7100f7e0
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 02:46:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli, other_deli, another_deli)
   if katz_deli
     puts "The line is currently empty."
   elsif other_deli
-    puts "The line is currently"
+    puts "The line is currently: 1. #{other_deli[0]}"
   end
 
 end
