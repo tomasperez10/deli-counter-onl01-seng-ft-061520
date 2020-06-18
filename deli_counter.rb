59cd9bb183777654c4bfdddf051b156888a307e5
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 02:48:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,8 @@ def line(katz_deli, other_deli, another_deli)
     puts "The line is currently empty."
   elsif other_deli
     puts "The line is currently: 1. #{other_deli[0]} 2. #{other_deli[1]} 3. #{other_deli[2]}"
+  else
+    puts "The line is currently: 1. #{another_deli[0]} 2. #{another_deli[1]} 3. #{another_deli[2]} 4. #{another_deli[4]} 5. #{another_deli[5]}"
   end
 
 end
