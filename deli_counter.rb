51cfaebcaa186b47f58ad320b8140b7d4c744a84
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 02:57:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,4 +8,4 @@ def line(array)
       new_array.push("#{counter}. #{name}")
       counter += 1
     end
-    puts "The line is currently: #{new_array.join
+    puts "The line is currently: #{new_array.join(" ")
